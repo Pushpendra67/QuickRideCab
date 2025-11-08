634870bec951996e6db06ccdfc8f4b15d53efb28
Pushpendra Singh <pushpendrasingh@clarivate.com>
2025-11-08 10:38:02 +0530

    addcors

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const http = require('http');
 const app = require('./app');
+const cors = require("cors");
+
 const { initializeSocket } = require('./socket');
+
+app.use(cors());
+
+app.use(express.json());
 const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
--- socket.js
+++ socket.js
@@ -8,7 +8,9 @@ function initializeSocket(server) {
     io = socketIo(server, {
         cors: {
             origin: '*',
-            methods: [ 'GET', 'POST' ]
+            methods: [ 'GET', 'POST' ],
+            allowedHeaders: ["Content-Type", "Authorization"],
+            credentials: true
         }
     });
 
